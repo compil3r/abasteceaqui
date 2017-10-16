824b15d508932d204fd08b074aca77106f028c21
Vitor Hugo Lopes <vitor@youniverseweb.com>
2017-10-16 18:53:46 -0200

    add pagina sobre

--- js/abasteceaqui.js
+++ js/abasteceaqui.js
@@ -35,4 +35,20 @@
     }
   });
 
+
+  function sticky_footer() {
+        var mFoo = $("#footer");
+        if ((($(document.body).height() + mFoo.outerHeight()) < $(window).height() && mFoo.css("position") == "fixed") || ($(document.body).height() < $(window).height() && mFoo.css("position") != "fixed")) {
+            mFoo.css({ position: "fixed", bottom: "0px" ,width: '100%'});
+        } else {
+            mFoo.css({ position: "absolute" });
+        }
+    }
+
+    jQuery(document).ready(function($){
+        sticky_footer();
+        $(window).resize(sticky_footer);
+        $(window).load(sticky_footer);
+    });
+
 })(jQuery); // End of use strict
